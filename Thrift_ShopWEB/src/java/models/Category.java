051da53a9a4418bd92c812e05d2bed9c2dec3a17
LAPTOP-E01ARKT8\Le Nguyen Nhat Minh <lenguyennhatminh0107@gmail.com>
2022-10-05 17:00:13 +0700

    Category + DAO

--- Thrift_ShopWEB/src/java/models/Category.java
+++ Thrift_ShopWEB/src/java/models/Category.java
@@ -34,4 +34,9 @@ public class Category {
         this.categoryName = categoryName;
     }
 
+    @Override
+    public String toString() {
+        return "Category{" + "categoryID=" + categoryID + ", categoryName=" + categoryName + '}';
+    }
+    
 }
